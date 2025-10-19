55c65d82a1e019ba4923256741059b215d3229d2
Deven <devenpuri29@gmail.com>
2025-10-20 02:41:09 +0530

    Add JS Promises practice files - callbacks, promises, and HTML demo

---
+++ JS-Promises/callbacks.js
@@ -0,0 +1,24 @@
+const post = [
+    { title: 'Post One', body: 'This is post one'},
+    {title: 'Post Two', body: 'This is post two'}
+];
+
+function getposts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post , index) => {
+            output += `<li>${post.title}</li>`;
+        })
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post , callback) {
+    setTimeout(() => {
+        posts.push(post);
+    } , 1000);
+}
+
+getposts();
+
+createPost({title: 'Post Three', body: 'This is post three'} , getposts);
